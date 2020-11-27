890bc278458a0d89f762b0615f6f7e5dca156e03
cseandro <49765763+cseandro@users.noreply.github.com>
2020-11-27 07:47:23 +0530

    Create ytlive-embed.js

---
+++ ytlive-embed.js
@@ -0,0 +1,3 @@
+<script>
+document.write('<iframe width="'+width+'" height="'+height+'" src="https://www.youtube.com/embed/live_stream?channel='+id+'" frameborder="0" allow="accelerometer; autoplay; modestbranding; encrypted-media; gyroscope; picture-in-picture" frameborder="0"  style="margin:0px;padding:0px;overflow:hidden" allowfullscreen></iframe>');
+</script>
